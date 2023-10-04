502d099e1e630b58450c2159fa608a5dfda7d2db
Đạt <datlowashere@gmail.com>
2023-10-04 15:50:22 +0700

    Update routeQuanLyPhim.js
    update routequanlyphim.js

--- src/routers/routeQuanLyPhim.js
+++ src/routers/routeQuanLyPhim.js
@@ -3,9 +3,12 @@ const router=express.Router();
 
 const qlPhim=require('../app/controller/quanLyPhim');
 
-router.get('/phim/quanlyphim',qlPhim.getDSPhim);
+router.get('/quanlyphim/phim',qlPhim.dsPhim);
+router.get('/quanlyphim/phimdachieu',qlPhim.dsPhimDaChieu);
 router.get('/login',qlPhim.gotoLogin)
-router.get('/tongquan',qlPhim.goToTQ)
+router.get('/tongquan',qlPhim.tongQuan)
+router.get('/quanlyphim/phim/them',qlPhim.themPhim)
+
 
 
 
