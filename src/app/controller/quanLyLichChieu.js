6b7a28b930669dfbcf2378369cb7096bfc435ff1
Đạt <datlowashere@gmail.com>
2023-10-07 12:44:16 +0700

    Update quanLyLichChieu.js

--- src/app/controller/quanLyLichChieu.js
+++ src/app/controller/quanLyLichChieu.js
@@ -1,9 +1,9 @@
 class lichChieu{
     async dsLichChieu(req,res){
-        res.render('lichChieu', { title: 'Lịch Chiếu Phim' })
+        res.render('showtimes/lichChieu', { title: 'Lịch Chiếu Phim' })
     }
     async themLichChieu(req,res){
-        res.render('themLichChieu', { title: 'Thêm Lịch Chiếu Phim' })
+        res.render('showtimes/themLichChieu', { title: 'Thêm Lịch Chiếu Phim' })
     }
 
 }
