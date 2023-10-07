57c55a564ca832f2984c657f0c9749845272ef3f
Đạt <datlowashere@gmail.com>
2023-10-07 12:44:14 +0700

    Update quanLyLoaiPhim.js

--- src/app/controller/quanLyLoaiPhim.js
+++ src/app/controller/quanLyLoaiPhim.js
@@ -1,10 +1,10 @@
 class qlLoaiPhim{
 
     async dsLoaiPhim(req,res){
-        res.render('loaiPhim', { title: 'Loại Phim' });
+        res.render('movies/loaiPhim', { title: 'Loại Phim' });
     }
     async themLoaiPhim(req,res){
-        res.render('themLoaiPhim', { title: 'Thêm Loại Phim' });
+        res.render('movies/themLoaiPhim', { title: 'Thêm Loại Phim' });
     }    
 
 }
