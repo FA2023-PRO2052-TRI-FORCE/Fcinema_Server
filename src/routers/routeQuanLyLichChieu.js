a3c9d34f50a4ec30f001b5c4191a42e9f41f1405
Đạt <datlowashere@gmail.com>
2023-10-12 08:22:42 +0700

    Update routeQuanLyLichChieu.js

--- src/routers/routeQuanLyLichChieu.js
+++ src/routers/routeQuanLyLichChieu.js
@@ -3,8 +3,15 @@ const router=express.Router();
 
 const qlLichChieu=require('../app/controller/quanLyLichChieu');
 
-router.get('/lichchieu',qlLichChieu.dsLichChieu);
-router.get('/lichchieu/them',qlLichChieu.themLichChieu);
+router.get('/lichchieu',qlLichChieu.getAllLichChieu);
+router.get('/lichchieu/them',qlLichChieu.getNewLichChieu);
+router.post('/lichchieu/them/luu',qlLichChieu.addNewLichChieu);
+router.get('/lichchieu/sua/:idLichChieu',qlLichChieu.getChiTietLichChieu);
+router.post('/lichchieu/tim/:tenPhim',qlLichChieu.searchLichChieu);
+router.put('/lichchieu/sua/luu/:idLichChieu',qlLichChieu.updateLichChieu);
+router.put('/lichchieu/luutru/:idLichChieu',qlLichChieu.deleteLichChieu);
+
+
 
 
 module.exports=router;
