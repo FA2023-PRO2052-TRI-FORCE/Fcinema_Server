4da0e94ec75bad26887cac533a7caa6f2bd3a167
Đạt <datlowashere@gmail.com>
2023-10-12 14:37:49 +0700

    Update routeAPP.js

--- src/routers/routeAPP.js
+++ src/routers/routeAPP.js
@@ -1,3 +1,17 @@
-// /apiApp/taikhoan/dangnhap
+const express=require('express');
+const router=express.Router();
+const taiKhoanND=require('../app/controller/apiApp/taiKhoanController');
+
+router.post('/nguoidung/dangky',taiKhoanND.registerNguoiDung);
+router.post('/nguoidung/dangnhap',taiKhoanND.loginNguoiDung);
+router.post('/resetMatKhauRequest',taiKhoanND.resetMatKhauRequest);
+router.post('/comfirmResetMatKhau',taiKhoanND.comfirmResetMatKhau);
+router.get('/nguoidung/thongtin/:email',taiKhoanND.getThongTinNguoiDungByEmail);
+router.put('/nguoidung/doithongtin/:email',taiKhoanND.updateThongTinNguoiDung);
+router.put('/nguoidung/doimatkhau/:email',taiKhoanND.changePasswordByEmail);
+router.put('/nguoidung/xoaTaiKhoan',taiKhoanND.deleteTaiKhoan);
+
+
+module.exports=router;
 
 
