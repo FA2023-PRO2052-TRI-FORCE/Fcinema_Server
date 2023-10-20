ab6374ba76923fea3c254e4130a6a5d47f30189d
Đạt <datlowashere@gmail.com>
2023-10-21 01:27:09 +0700

    Create authMiddleware.js

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,13 @@
+module.exports = (req, res, next) => {
+  if (!req.session.user) {
+    return res.redirect('/login'); 
+  }
+
+
+  if (req.session.user[0].vaiTro !== 'admin') {
+    req.flash('notificationErr', 'Bạn không thể sử dụng chức năng này');
+    return res.redirect('/tongquan');
+
+  }
+  next();
+};
