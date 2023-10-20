f57fd0dc58655bd27cb9c1ab8d50abb1f2f6f12c
Đạt <datlowashere@gmail.com>
2023-10-21 01:27:32 +0700

    Update routeThongKe.js

--- src/routers/routeThongKe.js
+++ src/routers/routeThongKe.js
@@ -2,7 +2,8 @@ const express=require('express');
 const router=express.Router();
 
 const tk=require('../app/controller/thongKe');
+const authMiddleware=require('../middleware/authMiddleware');
 
-router.get('/thongke',tk.dsThongKe);
+router.get('/thongke',authMiddleware,tk.dsThongKe);
 
 module.exports=router;
