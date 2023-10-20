fcb4d1b6a0bc39695eb6a4dad0b085dab94fdaa7
Đạt <datlowashere@gmail.com>
2023-10-21 01:27:30 +0700

    Update routeQuanLyVeDat.js

--- src/routers/routeQuanLyVeDat.js
+++ src/routers/routeQuanLyVeDat.js
@@ -4,6 +4,10 @@ const router=express.Router();
 const veDat=require('../app/controller/quanLyVeDat');
 
 router.get('/ve',veDat.getAllVeDaDat);
-router.get('/ve/them',veDat.addNewVe);
+router.get('/ve/them',veDat.getNewVe);
+router.post('/ve/them/luu',veDat.addNewVe);
+router.get('/ve/chitiet/:idVe',veDat.getChiTetVe);
+router.get('/ve/them/ghe',veDat.getAllGheDaChon);
+
 
 module.exports=router;
