f4ad0f695ebaa470c694dbc7c2562b73b5114560
Đạt <datlowashere@gmail.com>
2023-10-21 01:27:23 +0700

    Update routeQuanLyLoaiPhim.js

--- src/routers/routeQuanLyLoaiPhim.js
+++ src/routers/routeQuanLyLoaiPhim.js
@@ -2,9 +2,10 @@ const express=require('express');
 const router=express.Router();
 
 const qlLoaiPhim=require('../app/controller/quanLyLoaiPhim');
+const authMiddleware=require('../middleware/authMiddleware');
 
-router.get('/quanlyphim/loaiphim',qlLoaiPhim.dsLoaiPhim);
-router.get('/quanlyphim/loaiphim/them',qlLoaiPhim.themLoaiPhim);
+router.get('/quanlyphim/loaiphim',authMiddleware,qlLoaiPhim.dsLoaiPhim);
+router.get('/quanlyphim/loaiphim/them',authMiddleware,qlLoaiPhim.themLoaiPhim);
 
 
 
