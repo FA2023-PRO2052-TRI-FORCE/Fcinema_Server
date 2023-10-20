98010ec920697dcc5fa64c4ed1a876bf6dced725
Đạt <datlowashere@gmail.com>
2023-10-21 01:27:19 +0700

    Update routeQLTK.js

--- src/routers/routeQLTK.js
+++ src/routers/routeQLTK.js
@@ -3,9 +3,12 @@ const router=express.Router();
 
 const qlThonTin=require('../app/controller/quanlythongtin');
 
-
+router.get('/login',qlThonTin.gotoLogin)
+router.get('/tongquan',qlThonTin.tongQuan)
 router.get('/managerAdmin',qlThonTin.goToManager)
 router.get('/changePassAdmin',qlThonTin.goToChangePass)
+router.post('/login-account',qlThonTin.loginAccount);
+router.put('/changePass/:idNhanVien',qlThonTin.changePasswordUser);
 
 
 
