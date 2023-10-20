f7fa7ccad1626ff781f6df15e82d663836018c74
DoannNguyen <nguyendoan27022001@gmail.com>
2023-10-20 14:39:48 +0700

    doan commit

---
+++ src/app/controller/apiApp/PhimController.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const connection = require("../../../config/connection");
+const getAllPhimDC = (req, res) => {
+  connection.query(
+    "select a.tenPhim, a.nuocSX, a.namSX, a.thoiLuong, a.ngonNgu," +
+      "a.idPhim, a.moTa, b.idLichChieu, b.giaPhim, b.ngayChieu, b.caChieu, c.tenTheLoai, d.idPhongChieu, d.tenPhongChieu from phim a join lichChieu b on a.idPhim = b.idPhim" +
+      " join theLoai c on a.idTheLoai = c.idTheLoai join phongChieu d on b.idPhongChieu = d.idPhongChieu where a.trangThai = 2 ",
+    (err, result) => {
+      if (err) throw err;
+      res.send(result);
+    }
+  );
+};
+
+const getAllPhimSC = (req, res) => {
+  connection.query(
+    "select a.anh, a.tenPhim, a.nuocSX, a.namSX, a.thoiLuong, a.ngonNgu, a.daoDien, b.tenTheLoai from phim a join theLoai b on a.idTheLoai = b.idTheLoai where trangThai = 1",
+    (err, result) => {
+      if (err) throw err;
+      res.send(result);
+    }
+  );
+};
+
+const getViTriGheById = (req, res) => {
+  const id = req.params.id;
+  console.log("check data: ", id);
+  connection.query(
+    "select a.tenGhe from vitrighe a join ve b on a.idve=b.idve join lichChieu c on b.idLichChieu = c.idLichChieu where c.idLichChieu = ?",
+    [id],
+    (err, result) => {
+      if (err) throw err;
+      // res.json({ data: result });
+      res.send(result);
+    }
+  );
+};
+module.exports = {
+  getAllPhimDC,
+  getAllPhimSC,
+  getViTriGheById,
+};
---
+++ src/app/controller/apiApp/VeController.js
@@ -0,0 +1,42 @@
+const { json } = require("body-parser");
+const connection = require("../../../config/connection");
+const { format } = require("date-fns");
+const postDatVe = (req, res) => {
+  const ve = req.body.mVeModel;
+  const ghe = req.body.mViTriGheModel;
+  const data = JSON.stringify(req.body.mJsonArray);
+  connection.query(
+    "insert into ve (idVe, soVe, ngayMua, tongTien, ngayThanhToan, trangThai, idLichChieu) values (?, ?, ?, ?, ?, ?, ?)",
+    [ve.idVe, ve.soVe, ve.ngayMua, ve.tongTien, ve.ngayTT, 1, ve.idLichChieu],
+    (err, result) => {
+      if (err) throw err;
+      else {
+        connection.query(
+          "insert into vitrighe (tenGhe, idPhongChieu, idVe, trangThai) values (?, ?, ?, ?)",
+          [data, ghe.idPhongChieu, ghe.idVe, 1],
+          (err, result) => {
+            if (err) throw err;
+            res.status(200).send("them du lieu thanh cong");
+          }
+        );
+      }
+    }
+  );
+};
+
+const getVeDat = (req, res) => {
+  connection.query(
+    "select a.tenPhim, b.giaPhim, b.caChieu, b.ngayChieu, d.tenPhongChieu, c.trangThai, c.idVe, c.soVe, C.tongTien, e.tenGhe " +
+      "from phim a join lichChieu b on a.idPhim = b.idPhim join ve c on b.idLichChieu=c.idLichChieu join phongChieu d on b.idPhongChieu = d.idPhongChieu " +
+      "join vitrighe e on e.idVe=c.idVe",
+    (err, result) => {
+      if (err) throw err;
+      res.send(result);
+    }
+  );
+};
+
+module.exports = {
+  postDatVe,
+  getVeDat,
+};
