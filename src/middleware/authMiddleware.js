371fcfbdcad0659c9d7eea9fee45481b01c1fdc9
Đạt <datlowashere@gmail.com>
2023-10-21 23:41:08 +0700

    Đạt update authMiddleware 21/10/23

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -3,11 +3,9 @@ module.exports = (req, res, next) => {
     return res.redirect('/login'); 
   }
 
-
   if (req.session.user[0].vaiTro !== 'admin') {
     req.flash('notificationErr', 'Bạn không thể sử dụng chức năng này');
     return res.redirect('/tongquan');
-
   }
   next();
 };
