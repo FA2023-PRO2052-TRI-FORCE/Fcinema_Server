e7dcddf1f87b41e345012781a5a08c8544b97224
Đạt <datlowashere@gmail.com>
2023-10-21 23:43:26 +0700

    Đạt update route quản lý phim 21/10/23

--- src/routers/routeQuanLyPhim.js
+++ src/routers/routeQuanLyPhim.js
@@ -4,16 +4,14 @@ const router=express.Router();
 const qlPhim=require('../app/controller/quanLyPhim');
 const authMiddleware=require('../middleware/authMiddleware');
 
-router.get('/quanlyphim/phim',authMiddleware,qlPhim.dsPhim);
-router.get('/quanlyphim/phimdachieu',authMiddleware,qlPhim.dsPhimDaChieu);
-router.get('/quanlyphim/phim/them',authMiddleware,qlPhim.themPhim)
+router.get('/quanlyphim/phim',authMiddleware,qlPhim.getdsPhim);
+router.get('/quanlyphim/phimdachieu',authMiddleware,qlPhim.getdsPhimDaChieu);
+router.get('/quanlyphim/phim/them',authMiddleware,qlPhim.getThemPhim)
 router.get('/quanlyphim/phimdachieu/:idPhim',authMiddleware,qlPhim.chiTietPhimDaChieu);
 router.put('/quanlyphim/phimdachieu/:idPhim',authMiddleware,qlPhim.xoaPhimDaChieu);
 
-
-router.get('/quanlyphim/phim/them',qlPhim.getThemPhim)
-router.post('/quanlyphim/phim/them/luu',qlPhim.themPhim)
-router.put('/quanlyphim/phim/xoa/:idPhim',qlPhim.xoaPhim)
-router.get('/quanlyphim/phim/capnhat/:idPhim',qlPhim.getCapNhatPhim)
+router.post('/quanlyphim/phim/them/luu',authMiddleware,qlPhim.themPhim)
+router.put('/quanlyphim/phim/xoa/:idPhim',authMiddleware,qlPhim.xoaPhim)
+router.get('/quanlyphim/phim/capnhat/:idPhim',authMiddleware,qlPhim.getCapNhatPhim)
 
 module.exports=router;
