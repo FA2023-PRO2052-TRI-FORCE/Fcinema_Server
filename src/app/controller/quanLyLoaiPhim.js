000377427fbc1b0be7c1f8bc74b09950db2911b5
Đạt <datlowashere@gmail.com>
2023-10-22 23:03:20 +0700

    Đạt update loại phim 22/10/23

--- src/app/controller/quanLyLoaiPhim.js
+++ src/app/controller/quanLyLoaiPhim.js
@@ -3,14 +3,21 @@ const express = require("express");
 
 class qlLoaiPhim {
   async dsLoaiPhim(req, res) {
+    const hoTenND=req.session.user[0].hoTen;
     connection.query("SELECT * FROM TheLoai WHERE hienThi = 1", (err, result) => {
         if (err) {
             console.error("Lỗi", err.message);
             return;
         }
         console.log(result);
+        const notificationSuccess = req.flash('notificationSuccess');
+        const notificationErr = req.flash('notificationErr'); 
         res.render("movies/loaiPhim", {
+            title:'Danh sách thể loại phim',
             listTheLoai: result,
+            hoTenND:hoTenND,
+            notificationErr,
+            notificationSuccess
         });
     });
   }
@@ -19,22 +26,32 @@ class qlLoaiPhim {
     const tenTheLoai = req.body.tenTheLoai;
     const hienThi = 1
 
-    connection.query(
-      "INSERT INTO TheLoai (tenTheLoai, hienThi) VALUES (?, ?)",
-      [tenTheLoai,hienThi],
-      (err, result) => {
-        if (err) {
-          req.flash(
-            "notificationErr",
-            "Lỗi khi thêm loại phim: " + err.message
-          );
-          res.redirect("/quanlyphim/loaiphim");
-        } else {
-          req.flash("notificationSuccess", "Thêm loại phim thành công");
-          res.redirect("/quanlyphim/loaiphim");
+    const checkQuery=`SELECT * FROM TheLoai WHERE tenTheLoai=?`;
+    connection.query(checkQuery,[tenTheLoai],(checkErr,checkResult)=>{
+        if(checkResult.length>0){
+            req.flash('notificationErr','Tên thể loại đã tồn tại');
+            res.redirect("/quanlyphim/loaiphim");
+            return;
         }
-      }
-    );
+
+        connection.query(
+            "INSERT INTO TheLoai (tenTheLoai, hienThi) VALUES (?, ?)",
+            [tenTheLoai,hienThi],
+            (err, result) => {
+              if (err) {
+                req.flash(
+                  "notificationErr",
+                  "Lỗi khi thêm loại phim: " + err.message
+                );
+                res.redirect("/quanlyphim/loaiphim");
+              } else {
+                req.flash("notificationSuccess", "Thêm loại phim thành công");
+                res.redirect("/quanlyphim/loaiphim");
+              }
+            }
+          );        
+    })
+
   }
 
   async xoaLoaiPhim(req, res) {
@@ -45,9 +62,9 @@ class qlLoaiPhim {
         [idTheLoai],
         (err, result) => {
             if (err) {
-                req.flash("notificationErr", "Lỗi khi vô hiệu hóa thể loại phim: " + err.message);
+                req.flash("notificationErr", "Lỗi khi xoá thể loại phim: " + err.message);
             } else {
-                req.flash("notificationSuccess", "Vô hiệu hóa thể loại phim thành công");
+                req.flash("notificationSuccess", "Xoá thể loại phim thành công");
             }
             res.redirect('/quanlyphim/loaiphim');
         }
@@ -58,6 +75,7 @@ class qlLoaiPhim {
   
   async getCapNhatLoaiPhim(req, res) {
     const idTheLoai = req.params.idTheLoai;
+    const hoTenND=req.session.user[0].hoTen;
 
     const query = "SELECT * FROM TheLoai WHERE idTheLoai = ? AND hienThi = 1";
     connection.query(query, [idTheLoai], (err, result) => {
@@ -69,10 +87,12 @@ class qlLoaiPhim {
                 console.log(result);
                 // Trả về trang cập nhật với thông tin thể loại phim
                 res.render("movies/capNhatLoaiPhim", {
-                    loaiPhim: result[0], // Chỉ lấy thông tin đầu tiên (nếu có)
+                    loaiPhim: result[0],
+                    hoTenND:hoTenND 
                 });
             } else {
-                res.status(404).send("Không tìm thấy thể loại phim");
+                req.flash("notificationErr", "Lỗi cập nhật ");
+                res.redirect('/quanlyphim/loaiphim');
             }
         }
     });
@@ -82,30 +102,32 @@ class qlLoaiPhim {
   async capNhatLoaiPhim(req, res) {
     const idTheLoai = req.params.idTheLoai;
     const tenTheLoai = req.body.tenTheLoai;
-
-    // Kiểm tra xem thể loại phim có hienThi = 1 hay không trước khi cập nhật
-    const checkQuery = "SELECT hienThi FROM TheLoai WHERE idTheLoai = ?";
-    connection.query(checkQuery, [idTheLoai], (err, result) => {
+    
+    const checkQuery = "SELECT * FROM TheLoai WHERE tenTheLoai = ?";
+    connection.query(checkQuery, [tenTheLoai], (err, result) => {
         if (err) {
             console.error(err);
-            res.status(500).send("Lỗi khi kiểm tra trạng thái thể loại");
+            req.flash("notificationErr", "Lỗi");
+            res.redirect('/quanlyphim/loaiphim');
             return;
         }
 
-        if (result && result[0] && result[0].hienThi === 1) {
-            // Cập nhật thông tin thể loại phim nếu nó đang hiển thị
-            const updateQuery = "UPDATE TheLoai SET tenTheLoai = ? WHERE idTheLoai = ?";
-            connection.query(updateQuery, [tenTheLoai, idTheLoai], (err, result) => {
-                if (err) {
-                    console.error(err);
-                    res.status(500).send("Lỗi khi cập nhật thể loại");
-                } else {
-                    res.redirect("/quanlyphim/loaiphim");
-                }
-            });
-        } else {
-            res.status(400).send("Không thể cập nhật thể loại không hiển thị");
-        }
+        if (result.length>0 && result[0].hienThi==1) {
+            req.flash("notificationErr", "Tên thể loại đã có");
+            res.redirect('/quanlyphim/loaiphim'); 
+            return; 
+        } 
+        const updateQuery = "UPDATE TheLoai SET tenTheLoai = ? WHERE idTheLoai = ?";
+        connection.query(updateQuery, [tenTheLoai, idTheLoai], (err, result) => {
+            if (err) {
+                console.error(err);
+                req.flash("notificationErr", "Lỗi cập nhật ");
+                res.redirect('/quanlyphim/loaiphim');
+            } else {
+                req.flash("notificationSuccess", "Cập nhật thành công");
+                res.redirect("/quanlyphim/loaiphim");
+            }
+        });         
     });
 }
 
