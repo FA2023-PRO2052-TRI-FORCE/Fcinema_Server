c17cd6158fc4f87d3da4ece2debb4f861cb4e4ae
DoannNguyen <nguyendoan27022001@gmail.com>
2023-11-07 14:16:32 +0700

    Doan commit 07/11/23

--- src/app/controller/quanlythongtin.js
+++ src/app/controller/quanlythongtin.js
@@ -2,105 +2,103 @@ const e = require("express");
 const connection = require("../../config/connection");
 
 const upload = require("../../middleware/uploadService");
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 class quanlythongtin {
   // GET[]/taikhoan
-  async gotoAccount(req,res){
-    const hoTenND=req.session.user[0].hoTen;
-    const anhND=req.session.user[0].anh;
+  async gotoAccount(req, res) {
+    const hoTenND = req.session.user[0].hoTen;
+    const anhND = req.session.user[0].anh;
 
-    const idNhanVien=req.session.user[0].idNhanVien; 
-    const querry=`SELECT * FROM NhanVien WHERE idNhanVien=?`;
-    connection.query(querry,[idNhanVien],(err,results)=>{
+    const idNhanVien = req.session.user[0].idNhanVien;
+    const querry = `SELECT * FROM NhanVien WHERE idNhanVien=?`;
+    connection.query(querry, [idNhanVien], (err, results) => {
+      const notificationSuccess = req.flash("notificationSuccess");
+      const notificationErr = req.flash("notificationErr");
+      const objNV = JSON.parse(JSON.stringify(results));
+      console.log("type of", typeof objNV.ngaySinh);
+      res.render("account/myAccount", {
+        title: "Thông tin tài khoản",
+        hoTenND: hoTenND,
+        anhND: anhND,
+        objNhanVien: objNV,
+        notificationSuccess: notificationSuccess,
+        notificationErr: notificationErr,
+      });
+    });
+  }
 
-      const notificationSuccess = req.flash('notificationSuccess');
-      const notificationErr = req.flash('notificationErr');
-      const objNV=JSON.parse(JSON.stringify(results));
-      console.log('type of',typeof objNV.ngaySinh);
-      res.render('account/myAccount', { 
-        title: 'Thông tin tài khoản',
-        hoTenND:hoTenND,
-        anhND:anhND,
-        objNhanVien:objNV, 
-        notificationSuccess:notificationSuccess,
-        notificationErr:notificationErr,
-      })
-  })
-}
+  // POST[]/login
+  async loginAccount(req, res) {
+    const idNhanVien = req.body.idNhanVien;
+    const matKhau = req.body.matKhau;
+    let message = [];
 
-    // POST[]/login
-    async loginAccount(req,res){
-        const idNhanVien=req.body.idNhanVien;
-        const matKhau=req.body.matKhau;
-        let message=[];
-        
-        const querry=`SELECT *  FROM NhanVien WHERE idNhanVien=? and hienThi=1`;
-        const values=[idNhanVien,matKhau];
-        connection.query(querry,values,(err,results)=>{
-            if(err){
-                console.error('Lỗi',err.message);
-                return;
-            }
-            if(results.length==0){
-                message.push({err:'Người dùng không tồn tại'});
-                res.render('account/login', { layout: 'login',
-                message:message ,
-                idNhanVien:req.body.idNhanVien,
-                matKhau: req.body.matKhau});
-            }else{
-                const storedPassword = results[0].matKhau;
-                if(matKhau!==storedPassword){
-                    message.push({err:'Mật khẩu không đúng'});
-                    res.render('account/login', { layout: 'login',
-                    message:message,
-                    idNhanVien:req.body.idNhanVien,
-                    matKhau: req.body.matKhau});                    
-                }else{
-                    const objectNV=JSON.parse(JSON.stringify(results));   
-                    req.session.user =objectNV;
-                    req.flash('notificationSuccess', 'Đăng nhập thành công');
-                    res.redirect('/tongquan');
-
-                }
-            }
-            
-            
-        })        
-    }
+    const querry = `SELECT *  FROM NhanVien WHERE idNhanVien=? and hienThi=1`;
+    const values = [idNhanVien, matKhau];
+    connection.query(querry, values, (err, results) => {
+      if (err) {
+        console.error("Lỗi", err.message);
+        return;
+      }
+      if (results.length == 0) {
+        message.push({ err: "Người dùng không tồn tại" });
+        res.render("account/login", {
+          layout: "login",
+          message: message,
+          idNhanVien: req.body.idNhanVien,
+          matKhau: req.body.matKhau,
+        });
+      } else {
+        const storedPassword = results[0].matKhau;
+        if (matKhau !== storedPassword) {
+          message.push({ err: "Mật khẩu không đúng" });
+          res.render("account/login", {
+            layout: "login",
+            message: message,
+            idNhanVien: req.body.idNhanVien,
+            matKhau: req.body.matKhau,
+          });
+        } else {
+          const objectNV = JSON.parse(JSON.stringify(results));
+          req.session.user = objectNV;
+          req.flash("notificationSuccess", "Đăng nhập thành công");
+          res.redirect("/tongquan");
+        }
+      }
+    });
+  }
   // GET[]/logout
-  async logoutAccount(req,res){
+  async logoutAccount(req, res) {
     if (req.session.user) {
       req.session.destroy();
-    }    
-    res.redirect('/login')
-
+    }
+    res.redirect("/login");
   }
   // GET[]/login
   async gotoLogin(req, res) {
-    const updateVeQuerry=`UPDATE VE v INNER JOIN lichchieu l ON v.idLichChieu = l.idLichChieu SET v.trangThai=2 WHERE l.ngayChieu < CURRENT_DATE`;
-    connection.query(updateVeQuerry,(err,result)=>{
-      if(err){
-        console.log('Lỗi',err.message)
+    const updateVeQuerry = `UPDATE VE v INNER JOIN lichchieu l ON v.idLichChieu = l.idLichChieu SET v.trangThai=2 WHERE l.ngayChieu < CURRENT_DATE`;
+    connection.query(updateVeQuerry, (err, result) => {
+      if (err) {
+        console.log("Lỗi", err.message);
       }
       res.render("account/login", { layout: "login" });
-
-    })
+    });
   }
   // GET[]/changepass
   async goToChangePass(req, res) {
     const matKhau = req.session.user[0].matKhau;
     const idNhanVien = req.session.user[0].idNhanVien;
-    const hoTenND=req.session.user[0].hoTen;
-    const anhND=req.session.user[0].anh;
+    const hoTenND = req.session.user[0].hoTen;
+    const anhND = req.session.user[0].anh;
 
     const notificationSuccess = req.flash("notificationSuccess");
     const notificationErr = req.flash("notificationErr");
     res.render("account/changePass", {
       title: "Thay đổi mật khẩu",
       hoTenND: hoTenND,
-      anhND:anhND,
+      anhND: anhND,
       matKhau: matKhau,
       idNhanVien: idNhanVien,
       notificationErr,
@@ -144,7 +142,6 @@ class quanlythongtin {
     const hoTenND = req.session.user[0].hoTen;
     const anhND = req.session.user[0].anh;
 
-
     const querry = `UPDATE LichChieu SET hienThi=0  WHERE ngayChieu<= CURRENT_DATE `;
     connection.query(querry, (err, results) => {
       if (err) {
@@ -160,7 +157,7 @@ class quanlythongtin {
         res.render("account/dasboard", {
           title: "Tổng Quan",
           hoTenND: hoTenND,
-          anhND:anhND,
+          anhND: anhND,
           notificationSuccess,
           notificationErr,
         });
@@ -177,7 +174,7 @@ class quanlythongtin {
         const idNhanVien = req.session.user[0].idNhanVien;
         const hoTen = req.body.hoTen;
         const dienThoai = req.body.dienThoai;
-        const email=req.body.email;
+        const email = req.body.email;
         const ngaySinh = req.body.ngaySinh;
         const diaChi = req.body.diaChi;
         const gioiTinh = req.body.gioiTinh;
@@ -185,21 +182,19 @@ class quanlythongtin {
         var anhStringBase64;
         var emailRegex = /^\S+@\S+\.\S+$/;
         var dienThoaiRegex = /^(\+84|0)[1-9]\d{8}$/;
-  
+
         if (!emailRegex.test(email)) {
           req.flash("notificationErr", "Email định dạng không đúng");
-          return res.redirect('/myAccount');
-        }
-  
-        if (!dienThoaiRegex.test(dienThoai)) {
-          req.flash("notificationErr", "Điện thoại định dạng không đúng");
-          return res.redirect('/myAccount');
+          return res.redirect("/myAccount");
         }
 
+        if (!dienThoaiRegex.test(dienThoai)) {
+          req.flash("notificationErr", "Điện thoại định dạng không đúng");
+          return res.redirect("/myAccount");
+        }
 
         let query;
         let params;
-      
 
         if (req.file) {
           var anh = fs.readFileSync(req.file.path);
@@ -217,7 +212,6 @@ class quanlythongtin {
             idNhanVien,
           ];
           req.session.user[0].anh = anhStringBase64;
-
         } else {
           query = `
                 UPDATE NhanVien SET hoTen = ?, dienThoai = ?, email= ?, ngaySinh = ?, diaChi = ?, gioiTinh = ? WHERE idNhanVien = ? `;
@@ -235,18 +229,18 @@ class quanlythongtin {
           if (err) {
             console.error("Lỗi", err.message);
             req.flash("notificationErr", "Lỗi");
-            res.redirect('/myAccount');
+            res.redirect("/myAccount");
             return;
-          }      
+          }
 
           req.session.user[0].hoTen = hoTen;
           req.session.user[0].dienThoai = dienThoai;
           req.session.user[0].email = email;
           req.session.user[0].ngaySinh = ngaySinh;
           req.session.user[0].diaChi = diaChi;
-          req.session.user[0].gioiTinh = gioiTinh;          
+          req.session.user[0].gioiTinh = gioiTinh;
           req.flash("notificationSuccess", "Thay đổi thông tin thành công");
-          res.redirect('/myAccount');
+          res.redirect("/myAccount");
         });
       }
     });
