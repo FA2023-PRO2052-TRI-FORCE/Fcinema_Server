f50f82208566cbd1c54b610567724eab5e48b1be
DoannNguyen <nguyendoan27022001@gmail.com>
2023-11-07 13:40:01 +0700

    doan commit

--- index.js
+++ index.js
@@ -1,55 +1,56 @@
-const express = require('express');
-const handlebars = require('express-handlebars');
-const path = require('path');
-const morgan = require('morgan');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
-const moment = require('moment');
-const paginateHelper = require('express-handlebars-paginate');
-const flash = require('express-flash');
-const session = require('express-session');
-
+const express = require("express");
+const handlebars = require("express-handlebars");
+const path = require("path");
+const morgan = require("morgan");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+const moment = require("moment");
+const paginateHelper = require("express-handlebars-paginate");
+const flash = require("express-flash");
+const session = require("express-session");
 
 const app = express();
 const port = 3000;
 
-const route=require('./src/routers/index');
+const route = require("./src/routers/index");
 
-
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, 'src')));
-app.use(express.static(path.join(__dirname, 'src', 'resources')));
+app.use(express.static(path.join(__dirname, "src")));
+app.use(express.static(path.join(__dirname, "src", "resources")));
 
 // http logger
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
-app.use(session({
-  secret: 'get-message',
-  resave: true,
-  saveUninitialized: true
-}));
+app.use(
+  session({
+    secret: "get-message",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 app.use(flash());
 
-
 app.engine(
-  'hbs',
+  "hbs",
   handlebars.engine({
-    defaultLayout: 'main',
-    extname: '.hbs',
-    layoutsDir: __dirname + '\\src\\views\\layouts\\',
-    partialsDir: __dirname + '\\src\\views\\',
-    runtimeOptions:{allowProtoPropertiesByDefault:true,
-    allowedProtoMethodsByDefault:true},        
+    defaultLayout: "main",
+    extname: ".hbs",
+    layoutsDir: __dirname + "\\src\\views\\layouts\\",
+    partialsDir: __dirname + "\\src\\views\\",
+    runtimeOptions: {
+      allowProtoPropertiesByDefault: true,
+      allowedProtoMethodsByDefault: true,
+    },
     helpers: {
       sum: (a, b) => a + b,
-      formatDate:(date) => moment(date).format('DD/MM/YYYY'),
+      formatDate: (date) => moment(date).format("DD/MM/YYYY"),
       paginate: paginateHelper.createPagination,
       formatCurrency: (number, currencyCode) => {
-        const formatter = new Intl.NumberFormat('vi-VN', {
-          style: 'currency',
+        const formatter = new Intl.NumberFormat("vi-VN", {
+          style: "currency",
           currency: currencyCode,
         });
         return formatter.format(number);
@@ -57,32 +58,29 @@ app.engine(
       eq(val1, val2, options) {
         return val1 === val2 ? options.fn(this) : options.inverse(this);
       },
-      json:function (context) {
+      json: function (context) {
         return JSON.stringify(context);
       },
-      object:function (context) {
+      object: function (context) {
         return JSON.parse(context);
       },
-      convertTT:function(trangThai) {
-        return trangThai === 1 ? 'Đang hoạt động' : 'Không hoạt động';
+      convertTT: function (trangThai) {
+        return trangThai === 1 ? "Đang hoạt động" : "Không hoạt động";
       },
-      bufferToBlob:function (buffer, mimeType) {
+      bufferToBlob: function (buffer, mimeType) {
         const blob = new Blob([buffer], { type: mimeType });
         return URL.createObjectURL(blob);
       },
-  
     },
   })
 );
 
-
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '\\src\\views\\');
+app.set("view engine", "hbs");
+app.set("views", __dirname + "\\src\\views\\");
 
 route(app);
 
 app.listen(port, () => {
-  console.log('port: ' + port);
+  console.log("port: " + port);
   console.log("PATH:" + __dirname + "\\src\\views");
-
 });
--- src/app/controller/quanlythongtin.js
+++ src/app/controller/quanlythongtin.js
@@ -85,138 +85,140 @@ class quanlythongtin {
     if (req.session.user) {
       req.session.destroy();
     }
-<<<<<<< Updated upstream
+    /////Updated upstream
     // GET[]/login
-    async gotoLogin(req,res){
-        
-        res.render('account/login', { layout: 'login' });
-    }    
+    gotoLogin = (req, res) => {
+      res.render("account/login", { layout: "login" });
+    };
     // GET[]/changepass
-    async goToChangePass(req,res){
-        const matKhau=req.session.user[0].matKhau;
-        const idNhanVien=req.session.user[0].idNhanVien;
-        const hoTenND=req.session.user[0].hoTen;
-        const notificationSuccess = req.flash('notificationSuccess');
-        const notificationErr = req.flash('notificationErr');        
-        res.render('account/changePassAdmin', { title: 'Thay đổi mật khẩu',
-        hoTenND:hoTenND,
-        matKhau:matKhau,
-        idNhanVien:idNhanVien,
-        notificationErr
-        ,notificationSuccess })
-    }
+    goToChangePass = (req, res) => {
+      const matKhau = req.session.user[0].matKhau;
+      const idNhanVien = req.session.user[0].idNhanVien;
+      const hoTenND = req.session.user[0].hoTen;
+      const notificationSuccess = req.flash("notificationSuccess");
+      const notificationErr = req.flash("notificationErr");
+      res.render("account/changePassAdmin", {
+        title: "Thay đổi mật khẩu",
+        hoTenND: hoTenND,
+        matKhau: matKhau,
+        idNhanVien: idNhanVien,
+        notificationErr,
+        notificationSuccess,
+      });
+    };
     // PUT[]/changepass
-    async changePasswordUser(req,res){
-        const idNhanVien=req.session.user[0].idNhanVien; 
-        const oldMatKhau=req.session.user[0].matKhau; 
-        const matKhau=req.body.matKhau;
-        const newMatKhau=req.body.newMatKhau;
-        const comfirmMatKhau=req.body.comfirmMatKhau;
-        const updateQuerry =`UPDATE NhanVien SET matKhau=? WHERE idNhanVien=?`;  
-        connection.query(updateQuerry,[newMatKhau,idNhanVien],(err,results)=>{
-            if(err) {
-                req.flash('notificationErr', 'Không tìm thấy phim');
-                res.redirect('/changePassAdmin');
-                return;
-            }
-            if(matKhau !=oldMatKhau){
-                req.flash('notificationErr', 'Mật khẩu cũ không đúng');
-                res.redirect('/changePassAdmin');
-                return;                
-            }
-            if(newMatKhau !=comfirmMatKhau){
-                req.flash('notificationErr', 'Xác nhận mật khẩu mới không trùng');
-                res.redirect('/changePassAdmin');
-                return;
-            }else{
-                req.flash('notificationSuccess', 'Thay đổi mật khẩu thành công');
-                res.redirect('/changePassAdmin');
-            }
-
-        })      
-
-    }
-
+    changePasswordUser = (req, res) => {
+      const idNhanVien = req.session.user[0].idNhanVien;
+      const oldMatKhau = req.session.user[0].matKhau;
+      const matKhau = req.body.matKhau;
+      const newMatKhau = req.body.newMatKhau;
+      const comfirmMatKhau = req.body.comfirmMatKhau;
+      const updateQuerry = `UPDATE NhanVien SET matKhau=? WHERE idNhanVien=?`;
+      connection.query(
+        updateQuerry,
+        [newMatKhau, idNhanVien],
+        (err, results) => {
+          if (err) {
+            req.flash("notificationErr", "Không tìm thấy phim");
+            res.redirect("/changePassAdmin");
+            return;
+          }
+          if (matKhau != oldMatKhau) {
+            req.flash("notificationErr", "Mật khẩu cũ không đúng");
+            res.redirect("/changePassAdmin");
+            return;
+          }
+          if (newMatKhau != comfirmMatKhau) {
+            req.flash("notificationErr", "Xác nhận mật khẩu mới không trùng");
+            res.redirect("/changePassAdmin");
+            return;
+          } else {
+            req.flash("notificationSuccess", "Thay đổi mật khẩu thành công");
+            res.redirect("/changePassAdmin");
+          }
+        }
+      );
+    };
 
     // GET[]/tongquan
-    async tongQuan(req,res){
-        const notificationSuccess = req.flash('notificationSuccess');
-        const notificationErr = req.flash('notificationErr');
-        const hoTenND=req.session.user[0].hoTen;
+    tongQuan = (req, res) => {
+      const notificationSuccess = req.flash("notificationSuccess");
+      const notificationErr = req.flash("notificationErr");
+      const hoTenND = req.session.user[0].hoTen;
 
-        const querry=`UPDATE LichChieu SET hienThi=0  WHERE ngayChieu<= CURRENT_DATE `;
-        connection.query(querry,(err,results)=>{
-            if(err){
-                console.error('Lỗi',err.message);
-                return;
-            }
-            const querryP=`UPDATE Phim SET trangThai=0 WHERE idPhim IN(SELECT l.idPhim FROM LichChieu l WHERE l.ngayChieu < CURRENT_DATE)`;
-            connection.query(querryP,(err,result)=>{
-                if(err){
-                    console.error('Lỗi',err.message);
-                    return;
-                }                
-                res.render('account/dasboard', { 
-                    title: 'Tổng Quan',
-                    hoTenND:hoTenND,
-                    notificationSuccess,
-                    notificationErr });
-            })
-
-        })
-    }    
+      const querry = `UPDATE LichChieu SET hienThi=0  WHERE ngayChieu<= CURRENT_DATE `;
+      connection.query(querry, (err, results) => {
+        if (err) {
+          console.error("Lỗi", err.message);
+          return;
+        }
+        const querryP = `UPDATE Phim SET trangThai=0 WHERE idPhim IN(SELECT l.idPhim FROM LichChieu l WHERE l.ngayChieu < CURRENT_DATE)`;
+        connection.query(querryP, (err, result) => {
+          if (err) {
+            console.error("Lỗi", err.message);
+            return;
+          }
+          res.render("account/dasboard", {
+            title: "Tổng Quan",
+            hoTenND: hoTenND,
+            notificationSuccess,
+            notificationErr,
+          });
+        });
+      });
+    };
 
     // POST[]/login
-    async loginAccount(req,res){
-        const idNhanVien=req.body.idNhanVien;
-        const matKhau=req.body.matKhau;
-        let message=[];
-        
-        const querry=`SELECT *  FROM NhanVien WHERE idNhanVien=? and hienThi=1`;
-        const values=[idNhanVien,matKhau];
-        connection.query(querry,values,(err,results)=>{
-            if(err){
-                console.error('Lỗi',err.message);
-                return;
-            }
-            if(results.length==0){
-                message.push({err:'Người dùng không tồn tại'});
-                res.render('account/login', { layout: 'login',
-                message:message ,
-                idNhanVien:req.body.idNhanVien,
-                matKhau: req.body.matKhau});
-            }else{
-                const storedPassword = results[0].matKhau;
-                if(matKhau!==storedPassword){
-                    message.push({err:'Mật khẩu không đúng'});
-                    res.render('account/login', { layout: 'login',
-                    message:message,
-                    idNhanVien:req.body.idNhanVien,
-                    matKhau: req.body.matKhau});                    
-                }else{
-                    const objectNV=JSON.parse(JSON.stringify(results));   
-                    req.session.user =objectNV;
-                    req.flash('notificationSuccess', 'Đăng nhập thành công');
-                    res.redirect('/tongquan');
+    loginAccount = (req, res) => {
+      const idNhanVien = req.body.idNhanVien;
+      const matKhau = req.body.matKhau;
+      let message = [];
 
-                }
-            }
-            
-            
-        })        
-    }
+      const querry = `SELECT *  FROM NhanVien WHERE idNhanVien=? and hienThi=1`;
+      const values = [idNhanVien, matKhau];
+      connection.query(querry, values, (err, results) => {
+        if (err) {
+          console.error("Lỗi", err.message);
+          return;
+        }
+        if (results.length == 0) {
+          message.push({ err: "Người dùng không tồn tại" });
+          res.render("account/login", {
+            layout: "login",
+            message: message,
+            idNhanVien: req.body.idNhanVien,
+            matKhau: req.body.matKhau,
+          });
+        } else {
+          const storedPassword = results[0].matKhau;
+          if (matKhau !== storedPassword) {
+            message.push({ err: "Mật khẩu không đúng" });
+            res.render("account/login", {
+              layout: "login",
+              message: message,
+              idNhanVien: req.body.idNhanVien,
+              matKhau: req.body.matKhau,
+            });
+          } else {
+            const objectNV = JSON.parse(JSON.stringify(results));
+            req.session.user = objectNV;
+            req.flash("notificationSuccess", "Đăng nhập thành công");
+            res.redirect("/tongquan");
+          }
+        }
+      });
+    };
     // GET[]/logout
-    async logoutAccount(req,res){
-        if (req.session.user) {
-            req.session.destroy();
-        }    
-        res.redirect('/login')
+    logoutAccount = (req, res) => {
+      if (req.session.user) {
+        req.session.destroy();
+      }
+      res.redirect("/login");
+    };
 
-    }
-=======
     res.redirect("/login");
   }
->>>>>>> Stashed changes
+  /// Stashed changes
   async updateProfile(req, res) {
     const idNhanVien = req.session.user[0].idNhanVien;
     const hoTen = req.body.hoTen;
@@ -279,11 +281,11 @@ class quanlythongtin {
     });
   }
   // GET[]/login
-  async gotoLogin(req, res) {
+  gotoLogin = (req, res) => {
     res.render("account/login", { layout: "login" });
-  }
+  };
   // GET[]/changepass
-  async goToChangePass(req, res) {
+  goToChangePass = (req, res) => {
     const matKhau = req.session.user[0].matKhau;
     const idNhanVien = req.session.user[0].idNhanVien;
     const hoTenND = req.session.user[0].hoTen;
@@ -297,9 +299,9 @@ class quanlythongtin {
       notificationErr,
       notificationSuccess,
     });
-  }
+  };
   // PUT[]/changepass
-  async changePasswordUser(req, res) {
+  changePasswordUser = (req, res) => {
     const idNhanVien = req.session.user[0].idNhanVien;
     const oldMatKhau = req.session.user[0].matKhau;
     const matKhau = req.body.matKhau;
@@ -326,10 +328,10 @@ class quanlythongtin {
         res.redirect("/changePassAdmin");
       }
     });
-  }
+  };
 
   // GET[]/tongquan
-  async tongQuan(req, res) {
+  tongQuan = (req, res) => {
     const notificationSuccess = req.flash("notificationSuccess");
     const notificationErr = req.flash("notificationErr");
     const hoTenND = req.session.user[0].hoTen;
@@ -354,10 +356,10 @@ class quanlythongtin {
         });
       });
     });
-  }
+  };
 
   // POST[]/login
-  async loginAccount(req, res) {
+  loginAccount = (req, res) => {
     const idNhanVien = req.body.idNhanVien;
     const matKhau = req.body.matKhau;
     let message = [];
@@ -395,8 +397,8 @@ class quanlythongtin {
         }
       }
     });
-  }
-  async updateProfile(req, res) {
+  };
+  updateProfile = (req, res) => {
     upload.single("anh")(req, res, async function (err) {
       if (err) {
         // Xử lý lỗi tải lên hình ảnh ở đây
@@ -462,7 +464,7 @@ class quanlythongtin {
         });
       }
     });
-  }
+  };
 }
 
 module.exports = new quanlythongtin();
