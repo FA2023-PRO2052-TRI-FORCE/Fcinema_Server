8a836b403de54fa6a7257cb21714ba8e0e13557c
Đạt <datlowashere@gmail.com>
2023-11-15 10:31:26 +0700

    Đạt- Update route khách hàng-15/11/23

--- src/routers/routeKhachHang.js
+++ src/routers/routeKhachHang.js
@@ -4,9 +4,9 @@ const router=express.Router();
 const qlKhachHang=require('../app/controller/quanLyKhachHang');
 
 
-router.get('/khachhang',qlKhachHang.getAllKhachHang)
+router.get('/khachhang',qlKhachHang.getListKhachHang)
 router.get('/khachhang/:email',qlKhachHang.getKhachHangByEmail)
-
+router.put('/khachhang/:email',qlKhachHang.updateTrangThaiKhachHang)
 
 
 
