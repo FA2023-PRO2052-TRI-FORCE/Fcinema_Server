cde7d70bdd55e2074deb721916999223d9ebbaea
Đạt <datlowashere@gmail.com>
2023-11-30 08:24:23 +0700

    Đạt Update vé-lịch chiếu 30/11/2023

--- src/routers/routeQuanLyVeDat.js
+++ src/routers/routeQuanLyVeDat.js
@@ -5,9 +5,8 @@ const veDat=require('../app/controller/quanLyVeDat');
 
 router.get('/ve',veDat.getAllVeDaDat);
 router.get('/ve/them',veDat.getNewVe);
-router.post('/ve/them/luu',veDat.addNewVe);
-router.get('/ve/chitiet/:idVe',veDat.getChiTetVe);
-router.get('/ve/them/ghe',veDat.getAllGheDaChon);
+router.post('/ve/them/luu',veDat.insertNewVe);
+router.get('/ve/chitiet/:idVe',veDat.getChiTietVe);
 router.put('/ve/chitiet/:idVe',veDat.updateTrangThaiVe)
 
 
