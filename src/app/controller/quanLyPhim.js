54927aed829cd2a9894a8b52da7b23c049b19234
Đạt <datlowashere@gmail.com>
2023-12-04 03:20:34 +0700

    Đạt update phim 4/12/2023

--- src/app/controller/quanLyPhim.js
+++ src/app/controller/quanLyPhim.js
@@ -1,276 +1,206 @@
-const connection = require("../../config/connection");
+const PhimModel = require("../model/phimModel");
+const LoaiPhim = require("../model/loaiPhimModel");
 const upload = require("../../middleware/uploadService");
-const fs = require('fs');
-const path = require('path');
+const cloudinary = require("../../middleware/cloudinary");
+const phimModel = new PhimModel();
+const loaiPhim = new LoaiPhim();
+const fs = require("fs");
+const path = require("path");
+const defaultImg = path.join(__dirname,"../../resources/upload/cinema_logo_4x.png");
 
-let dsTheLoai = [];
-const defaultImg= path.join(__dirname,'../../resources/upload/cinema_logo_4x.png');
-
-  
 class quanLyPhim {
-  async getListPhim(req, res) {
-    const hoTenND=req.session.user[0].hoTen;
-    const anhND=req.session.user[0].anh;
+  //GET[]/phim
+  async getPhims(req, res) {
+    try {
+      const hoTenND = req.session.user[0].hoTen;
+      const anhND = req.session.user[0].anh;
+      const result = await phimModel.getListPhim();
 
-    const querry = `SELECT p.*, t.tenTheLoai FROM Phim p
-    INNER JOIN TheLoai t ON p.idTheLoai = t.idTheLoai
-    WHERE p.hienThi = 1 and p.trangThai=1;`;
+      const notificationSuccess = req.flash("notificationSuccess");
+      const notificationErr = req.flash("notificationErr");
 
-    connection.query(querry, async(err, result) => {
-      // kiểm tra truy vấn nếu có lỗi thì sẽ log lỗi ra và return không thực hiện các câu lệnh dưới
-      if (err) {
-        console.error("Lỗi", err.message);
-        return;
-      }
-      const notificationSuccess = req.flash('notificationSuccess');
-      const notificationErr = req.flash('notificationErr');      
       res.render("movies/phim", {
-        title:'Danh sách phim mới',
+        title: "Danh sách phim",
         listPhim: result,
-        hoTenND:hoTenND,
-        anhND:anhND,
+        hoTenND,
+        anhND,
         notificationErr,
         notificationSuccess
       });
-    });
+    } catch (error) {
+      console.error("Lỗi", error.message);
+      res.redirect("back");
+    }
   }
-  // GET[]/phim/phimdachieu
-  async getListPhimDC(req, res) {
-    const hoTenND=req.session.user[0].hoTen;
-    const anhND=req.session.user[0].anh;
-  
-
-    const querry = `SELECT C.ngayChieu, t.*, l.tenTheLoai from Phim t JOIN  theloai l ON t.idTheLoai= l.idTheLoai JOIN lichchieu c ON t.idPhim= c.idPhim WHERE c.ngayChieu < CURRENT_DATE ORDER BY c.ngayChieu DESC`;
-    connection.query(querry, (err, results) => {
-      if (err) {
-        console.error("Lỗi", err.message);
-        return;
-      }
-      res.render("movies/phimDaChieu", {
-        title: "Phim đã chiếu",
-        listDaChieu: results,
-        hoTenND:hoTenND,
-        anhND:anhND,
-        
-      });
-    });
-  }
-  // GET[]/phim/phimdachieu/:idPhim
-  async getChiTietPhimDaChieu(req, res) {
-    const idPhim = req.params.idPhim;
-    const hoTenND=req.session.user[0].hoTen;
-    const anhND=req.session.user[0].anh;
-
-    const querry = `SELECT l.tenTheLoai, p.* from Phim p, TheLoai l where p.idTheLoai=l.idTheLoai and trangThai=2 and idPhim=?`;
-    connection.query(querry, [idPhim], (err, results) => {
-      if (err) {
-        console.error("Lỗi", err.message);
-        return;
-      }
-      res.send(results);
-    });
-  }
-  // PUT[]/quanlyphim/phimdachieu/:idPhim
-  async xoaPhimDaChieu(req, res) {
-    const idPhim = req.params.idPhim;
-    const updateQuerry = `UPDATE PHIM SET hienThi=0 WHERE idPhim=?`;
-    connection.query(updateQuerry, [idPhim], (err, results) => {
-      if (err) {
-        console.error("Lỗi", err.message);
-        req.flash("notificationErr", "Xóa phim thất bại");
-        res.redirect('/quanlyphim/phimdachieu');        
-        return;
-      }
-      req.flash("notificationSuccess", "Xóa phim thành công");
-      res.redirect('/quanlyphim/phimdachieu');
-
-    });
-  }
-
 
   // GET/phim/them
   async getAddNewPhim(req, res) {
-    const hoTenND=req.session.user[0].hoTen;
-    const anhND=req.session.user[0].anh;
+    try {
+      const hoTenND = req.session.user[0].hoTen;
+      const anhND = req.session.user[0].anh;
 
-    const queryTheLoai = "SELECT * FROM TheLoai WHERE hienThi = 1";
+      const notificationSuccess = req.flash('notificationSuccess');
+      const notificationErr = req.flash('notificationErr');
+      const results = await loaiPhim.getAllLoaiPhim();
 
-    connection.query(queryTheLoai, (err, result) => {
-      dsTheLoai = result;
-      res.render("movies/themphim", { 
-        title:'Thêm phim mới',
-        listTheLoai: dsTheLoai,
-        hoTenND:hoTenND,
-        anhND:anhND,
+      res.render("movies/themphim", {
+        title: "Thêm phim mới",
+        listTheLoai: results,
+        hoTenND,
+        anhND,
+        notificationErr: notificationErr,
+        notificationSuccess: notificationSuccess,
       });
-    });
+    } catch (error) {
+      console.error("Lỗi", error.message);
+      res.status(500).send("Internal Server Error");
+    }
   }
 
-//   GET[]/dsTheloai
-  async getDanhSachTheLoai(req, res) {
-    return new Promise((resolve, reject) => {
-      connection.query("SELECT * FROM TheLoai", (err, results) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(results);
-        }
-      });
-    });
-  }
+  // GET/phim/capnhat/:idPhim
+  async getUpdatePhim(req, res) {
+    try {
+      const idPhim = req.params.idPhim;
+      const hoTenND = req.session.user[0].hoTen;
+      const anhND = req.session.user[0].anh;
 
+      const results = await phimModel.getPhimById(idPhim);
+
+      res.render("movies/suaPhim", {
+        title: "Cập nhật phim",
+        listPhim: JSON.parse(JSON.stringify(results)),
+        hoTenND: hoTenND,
+        anhND: anhND
+      });
+    } catch (error) {
+      console.error(error.message);
+      req.flash("notificationErr", "Lỗi: " + error.message);
+      res.redirect("/quanlyphim/phim");
+    }
+  }
 
   // POST[]/quanlyphim/phim/them/luu
-async addNewPhim(req, res) {
-  upload.single("anh")(req, res, async function (err) {
-    if (err) {
-      // Xử lý lỗi tải lên hình ảnh ở đây
-      console.error("Lỗi khi thêm ảnh: " + err);
-    } else {
-      const tenPhim = req.body.tenPhim;
+  async insertNewPhim(req, res) {
+    upload.single("anh")(req, res, async function (err) {
+      if (err) {
+        console.error("Lỗi khi thêm ảnh: " + err);
+      } else {
+        const tenPhim = req.body.tenPhim;
+        const ngonNgu = req.body.ngonNgu;
+        const moTa = req.body.moTa;
+        const hangSX = req.body.hangSX;
+        const nuocSX = req.body.nuocSX;
+        const namSX = req.body.namSX;
+        const thoiLuong = req.body.thoiLuong;
+        const daoDien = req.body.daoDien;
+        const dienVien = req.body.dienVien;
+        const idTheLoai = req.body.idTheLoai;
+        const ngayThem = new Date();
+        const dienVienArray = JSON.stringify(dienVien);
+
+        let anhUpload;
+
+        if (!req.file) {
+          req.flash("notificationErr", "Chưa chọn ảnh");
+          res.redirect("/quanlyphim/phim/them");
+          return;
+        }
+        try {
+
+          try {
+            const result = await cloudinary.uploader.upload(req.file.path, {
+              resource_type: "image",
+              folder: "storage/movie",
+            });
+            anhUpload = result.secure_url;
+          } catch (cloudinaryError) {
+            console.error("Lỗi khi tải ảnh lên Cloudinary:", cloudinaryError);
+            req.flash("notificationErr", "Lỗi khi tải ảnh lên Cloudinary");
+            res.redirect("/quanlyphim");
+            return;
+          }
+          const values = [tenPhim, anhUpload, ngonNgu, moTa, hangSX, nuocSX, namSX, thoiLuong, daoDien, dienVienArray, idTheLoai, ngayThem];
+          await phimModel.insertPhim(values);
+
+          req.flash("notificationSuccess", "Thêm phim mới thành công");
+          res.redirect("/quanlyphim/phim");
+        } catch (error) {
+          console.log(error.message);
+          req.flash("notificationErr", "Lỗi khi thêm phim: " + error.message);
+          res.redirect("/quanlyphim/phim");
+        }
+      }
+    });
+  }
+
+  // PUT/phim/capnhat/:idPhim
+  updatePhim(req, res) {
+    upload.single("anh")(req, res, async function (err) {
+
+      const idPhim = req.params.idPhim;
       const ngonNgu = req.body.ngonNgu;
       const moTa = req.body.moTa;
       const hangSX = req.body.hangSX;
       const nuocSX = req.body.nuocSX;
-      const namSX = req.body.namSX;
+      const anh = req.body.originalUrl;
       const thoiLuong = req.body.thoiLuong;
       const daoDien = req.body.daoDien;
-      const dienVien=req.body.dienVien;
-      const idTheLoai = req.body.idTheLoai;
-      const trangThai = 1;
-      const hienThi = 1;
-      const ngayThem=new Date()
-      const dienVienArray=JSON.stringify(dienVien);
-      
-      var anhStringBase64;
+      const dienVien = req.body.dienVien;
+      const dienVienArray = JSON.stringify(dienVien);
+      let anhUpload, publicId;
+      console.log('Anh', anh)
+
+
+      const publicIdMatch = anh.match(/\/v\d+\/(.+?)\.\w+$/);
 
       if (req.file) {
-        var anh = fs.readFileSync(req.file.path);
-        anhStringBase64 = anh.toString("base64");
-      } else {
-        var anhDefault=fs.readFileSync(defaultImg)
-        anhStringBase64 =anhDefault.toString("base64"); 
-      }  
-      connection.query(
-        "INSERT INTO Phim (tenPhim, anh, ngonNgu, moTa, hangSX, nuocSX, namSX, thoiLuong, daoDien, dienVien, hienThi, trangThai, idTheLoai,ngayThem) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-        [
-          tenPhim,
-          anhStringBase64,
-          ngonNgu,
-          moTa,
-          hangSX,
-          nuocSX,
-          namSX,
-          thoiLuong,
-          daoDien,
-          dienVienArray,
-          hienThi,
-          trangThai,
-          idTheLoai,
-          ngayThem
-        ],
-        (err, results) => {
-          if (err) {
-            console.log(err.message);
-            req.flash("notificationErr", "Lỗi khi thêm phim: " + err.message);
-            res.redirect("/quanlyphim/phim");
-          } else {
-            req.flash("notificationSuccess", "Thêm phim mới thành công");
-            res.redirect("/quanlyphim/phim");
+
+        if (publicIdMatch && publicIdMatch[1]) {
+          publicId = publicIdMatch[1];
+          if (publicId) {
+            await cloudinary.uploader.destroy(publicId);
           }
         }
-      );
-    }
-  });
-}
 
-//   PUT[]/phim/xoa/idPhim
+        try {
+          const result = await cloudinary.uploader.upload(req.file.path, {
+            resource_type: "image",
+            folder: "storage/movie",
+          });
+          anhUpload = result.secure_url;
+        } catch (cloudinaryError) {
+          console.error("Lỗi khi tải ảnh lên Cloudinary:", cloudinaryError);
+          req.flash("notificationErr", "Lỗi khi tải ảnh lên Cloudinary");
+          res.redirect("/baner");
+        }
+
+      }
+
+      await phimModel.updatePhim(idPhim, ngonNgu, moTa, daoDien, dienVienArray, hangSX, thoiLuong, nuocSX, anhUpload, (err, results) => {
+        if (err) {
+          console.error("Lỗi", err);
+          req.flash("notificationErr", "Lỗi");
+          res.redirect("/quanlyphim/phim");
+        } else {
+          req.flash("notificationSuccess", "Cập nhật phim thành công");
+          res.redirect("/quanlyphim/phim");
+        }
+      });
+    });
+  }
+  //   PUT[]/phim/xoa/idPhim
   async deletePhim(req, res) {
     const idPhim = req.params.idPhim;
 
-    connection.query(
-      "UPDATE Phim SET hienThi = 0 WHERE idPhim = ?",
-      [idPhim],
-      (err, result) => {
-        if (err) {
-          req.flash("notificationErr", "Xóa phim thất bại: ");
-        } else {
-          req.flash("notificationSuccess", "Xóa phim thành công");
-        }
-        res.redirect("/quanlyphim/phim");
-      }
-    );
+    try {
+      await phimModel.deletePhim(idPhim);
+      req.flash("notificationSuccess", "Xóa phim thành công");
+    } catch (err) {
+      req.flash("notificationErr", "Xóa phim thất bại: " + err.message);
+    }
+
+    res.redirect("/quanlyphim/phim");
   }
-
-// GET/phim/capnhat/:idPhim
-async getUpdatePhim(req, res) {
-  const idPhim = req.params.idPhim;
-  const hoTenND=req.session.user[0].hoTen;
-  const anhND=req.session.user[0].anh;
-  const queryPhim = "SELECT p.*, t.tenTheLoai FROM Phim p JOIN TheLoai t ON p.idTheLoai=t.idTheLoai WHERE p.idPhim = ? AND p.hienThi = 1";
-
-  connection.query(queryPhim,[idPhim],(err,results)=>{
-    res.render("movies/suaPhim", {
-      title: 'Cập nhật phim',
-      listPhim: JSON.parse(JSON.stringify(results)),
-      hoTenND: hoTenND,
-      anhND:anhND,
-    });
-  })
-
-}
-
-// PUT/phim/capnhat/:idPhim
-async updatePhim(req, res) {
-  upload.single("anh")(req, res, async function (err) {
-    if (err) {
-      console.error('Lỗi', err);
-      req.flash("notificationErr", "Lỗi");
-      res.redirect('/quanlyphim/phim');
-      return;
-    }
-
-    const idPhim = req.params.idPhim; 
-    const ngonNgu = req.body.ngonNgu;
-    const moTa = req.body.moTa;
-    const hangSX = req.body.hangSX;
-    const nuocSX = req.body.nuocSX;
-    const namSX = req.body.namSX;
-    const thoiLuong = req.body.thoiLuong;
-    const daoDien = req.body.daoDien;
-    const dienVien=req.body.dienVien;
-    let anhStringBase64 = null;
-    const dienVienArray=JSON.stringify(dienVien);
-
-    if (req.file) {
-      var anh = fs.readFileSync(req.file.path);
-      anhStringBase64 = anh.toString("base64");
-    }
-
-    let updateQuery;
-    let updateValues;
-
-    if (anhStringBase64) {
-      updateQuery = `UPDATE Phim SET ngonNgu=?, moTa=?, daoDien=?, dienVien=?, hangSX=?, thoiLuong=?, nuocSX=?, anh=? WHERE idPhim=?`;
-      updateValues = [ngonNgu, moTa, daoDien,dienVienArray, hangSX, thoiLuong, nuocSX, anhStringBase64, idPhim];
-    } else {
-      updateQuery = `UPDATE Phim SET ngonNgu=?, moTa=?, daoDien=?, dienVien=?, hangSX=?, thoiLuong=?, nuocSX=? WHERE idPhim=?`;
-      updateValues = [ngonNgu, moTa, daoDien,dienVienArray, hangSX, thoiLuong, nuocSX, idPhim];
-    }
-
-    connection.query(updateQuery, updateValues, (err, results) => {
-      if (err) {
-        console.error('Lỗi', err);
-        req.flash("notificationErr", "Lỗi");
-        res.redirect('/quanlyphim/phim');
-      } else {
-        req.flash("notificationSuccess", "Cập nhật phim thành công");
-        res.redirect('/quanlyphim/phim');
-      }
-    });
-  });
-}
-
 
 }
 module.exports = new quanLyPhim();
--- src/routers/routeQuanLyPhim.js
+++ src/routers/routeQuanLyPhim.js
@@ -4,13 +4,10 @@ const router=express.Router();
 const qlPhim=require('../app/controller/quanLyPhim');
 const authMiddleware=require('../middleware/authMiddleware');
 
-router.get('/quanlyphim/phim',authMiddleware,qlPhim.getListPhim);
-router.get('/quanlyphim/phimdachieu',authMiddleware,qlPhim.getListPhimDC);
-router.get('/quanlyphim/phimdachieu/:idPhim',authMiddleware,qlPhim.getChiTietPhimDaChieu);
-router.put('/quanlyphim/phimdachieu/:idPhim',authMiddleware,qlPhim.xoaPhimDaChieu);
+router.get('/quanlyphim/phim',authMiddleware,qlPhim.getPhims);
 
 router.get('/quanlyphim/phim/them',authMiddleware,qlPhim.getAddNewPhim)
-router.post('/quanlyphim/phim/them/luu',authMiddleware,qlPhim.addNewPhim)
+router.post('/quanlyphim/phim/them/luu',authMiddleware,qlPhim.insertNewPhim)
 router.put('/quanlyphim/phim/xoa/:idPhim',authMiddleware,qlPhim.deletePhim)
 router.get('/quanlyphim/phim/capnhat/:idPhim',authMiddleware,qlPhim.getUpdatePhim)
 router.put('/quanlyphim/phim/capnhat/:idPhim',authMiddleware,qlPhim.updatePhim);
