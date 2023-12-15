6ce71c49d07a94e1169edb8ee8d5d973355141e1
Đạt <datlowashere@gmail.com>
2023-12-16 05:06:42 +0700

    Đạt Update vé 16/12/2023

---
+++ src/public/js/autogenerateSeat.js
@@ -0,0 +1,75 @@
+// Xác định số hàng và số cột
+var rows = 4;
+var columns = 5;
+
+// Hàm tự động tạo ra ghế
+function generateSeatCheckbox(row, col) {
+    var seatId = String.fromCharCode(65 + row) + (col + 1);
+    var checkboxHtml = `
+    <div class="form-check" style="display: inline-block;">
+        <input class="form-check-input seat-checkbox" type="checkbox" value="${seatId}" id="${seatId}Checkbox">
+        <label class="form-check-label">
+            ${seatId}
+        </label>
+    </div>`;
+    document.write(checkboxHtml);
+}
+
+// Vòng lặp tự động tạo ghế layout
+for (var i = 0; i < rows; i++) {
+    document.write('<div class="d-flex justify-content-around">');
+    for (var j = 0; j < columns; j++) {
+        generateSeatCheckbox(i, j);
+    }
+    document.write('</div>');
+}
+// Check nếu như option "chonPhim" được chọn,(lúc đầu tải trang)
+var categorySelect = document.getElementById('category');
+var soLuongInputs = document.getElementsByName('soLuong');
+if (categorySelect.value === 'chonPhim') {
+    disableAllCheckboxes();
+    document.getElementById('img-preview').setAttribute("src", "../../resources/img/default_img.png");
+
+}
+
+// Thêm nghe sự kiện cho sự thay đổi lựa chọn phim
+categorySelect.addEventListener('change', function () {
+    if (this.value === 'chonPhim') {
+        disableAllCheckboxes();
+        disableAllSoLuongInputs();
+    } else {
+        enableAllCheckboxes();
+        enableAllSoLuongInputs();
+    }
+});
+
+// Hàm vô hiệu hóa tất cả các input soLuong
+function disableAllSoLuongInputs() {
+    soLuongInputs.forEach(function (input) {
+        input.disabled = true;
+    });
+}
+
+// Hàm kích hoạt tất cả các input soLuong
+function enableAllSoLuongInputs() {
+    soLuongInputs.forEach(function (input) {
+        input.disabled = false;
+    });
+}
+// Hàm vô hiệu hoá tất cả các checkbox ghế
+function disableAllCheckboxes() {
+    var checkboxes = document.querySelectorAll('.seat-checkbox');
+    checkboxes.forEach(function (checkbox) {
+        checkbox.disabled = true;
+    });
+}
+
+// Hàm kích hoạt tất cả các checkbox ghế
+function enableAllCheckboxes() {
+    var checkboxes = document.querySelectorAll('.seat-checkbox');
+    checkboxes.forEach(function (checkbox) {
+        checkbox.disabled = false;
+    });
+}
+
+
